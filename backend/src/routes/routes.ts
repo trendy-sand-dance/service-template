90505552426b1957545b4844233808328816569f
fvan-wij <fvan-wij@student.codam.nl>
2025-03-15 21:30:51 +0100

    Removed dir

--- backend/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import websocketController from '../controllers/websocket.controller.js';
-
-export async function routes(fastify: FastifyInstance) {
-  fastify.get('/ws-game', { websocket: true }, websocketController);
-  fastify.get('/', async function(request: FastifyRequest, reply: FastifyReply) {
-    reply.send({ message: 'HM from the Fastify backend', method: request.method });
-  });
-};
