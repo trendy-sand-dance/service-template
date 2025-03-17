fb6a713d74d6e8b2336b8709f904f8aa5ce519f9
fvan-wij <vanwijkflip@gmail.com>
2025-03-17 19:49:55 +0100

    Fix the fix

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,6 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 
 export async function routes(fastify: FastifyInstance) {
   fastify.get('/', async function(request: FastifyRequest, reply: FastifyReply) {
-    // reply.send({ message: 'Hi from the Fastify server', method: request.method });
-    return reply.sendFile('index.html');
+    reply.send({ message: 'Hi from the Fastify server', method: request.method });
   });
 };
