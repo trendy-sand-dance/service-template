f179128a8c2bdb7ec48d30d5a4e59be43b6ba7b1
fvan-wij <fvan-wij@student.codam.nl>
2025-03-20 23:03:17 +0100

    Made basic login screen

--- client/test.ts
+++ client/test.ts
@@ -1,4 +1,40 @@
-import { greet } from "./a.js";
 
-greet("Floop");
-greet("JOOOOOOOO KaP");
+console.log("Script loaded, waiting for interactions...");
+const regContainer = document.getElementById("register-container");
+const logContainer = document.getElementById("login-container");
+const toggleLogin = document.getElementById('toggleLogin')
+const toggleRegister = document.getElementById('toggleRegister')
+
+if (regContainer && logContainer) {
+  regContainer.style.display = "none";
+  toggleLogin?.classList.add("bg-teal-100");
+  // logContainer.style.display = "none";
+}
+let arr = [logContainer, regContainer];
+
+enum PageState {
+  Login = 0,
+  Register,
+};
+
+
+toggleLogin?.addEventListener('click', () => {
+  if (arr[PageState.Login] && arr[PageState.Register]) {
+    toggleLogin.classList.add("bg-teal-100");
+    toggleRegister?.classList.remove("bg-teal-100");
+    arr[PageState.Login].style.display = "block";
+    arr[PageState.Register].style.display = "none";
+  }
+})
+
+toggleRegister?.addEventListener('click', () => {
+  if (arr[PageState.Login] && arr[PageState.Register]) {
+    toggleRegister.classList.add("bg-teal-100");
+    toggleLogin?.classList.remove("bg-teal-100");
+    arr[PageState.Register].style.display = "block";
+    arr[PageState.Login].style.display = "none";
+  }
+})
+
+
+
